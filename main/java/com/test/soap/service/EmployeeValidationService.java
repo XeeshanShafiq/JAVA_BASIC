783bfc4d8648b9c1884e6dc27ea937d4fe18fd9d
xee1161 <68479691+xee1161@users.noreply.github.com>
2023-09-27 23:39:18 +0500

    Update EmployeeValidationService.java

--- main/java/com/test/soap/service/EmployeeValidationService.java
+++ main/java/com/test/soap/service/EmployeeValidationService.java
@@ -8,17 +8,30 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmployeeValidationService {
 
+
     public EmployeeValidationResponse validateEmployeeEmail(EmployeeValidationRequest request) {
+        EmployeeValidationResponse response = new EmployeeValidationResponse();
+        response.setTitle(checkExperience(request.getExperience()));
         String email = request.getEmail();
         boolean isValid = isValidEmail(email);
-
-        EmployeeValidationResponse response = new EmployeeValidationResponse();
         response.setIsValid(isValid);
-
         return response;
     }
 
     private boolean isValidEmail(String email) {
         return email != null;
     }
+
+    private String checkExperience(int experience){
+        if (experience > 0 && experience <= 2) {
+            return "The title for given experience is Intern";
+        } else if (experience > 2 && experience <= 5) {
+            return "The title for given experience is Developer";
+        } else if (experience > 5) {
+            return "The title for given experience is Lead";
+        } else {
+            return "Invalid Experience!!!! Please enter valid experience ";
+        }
+    }
+
 }
